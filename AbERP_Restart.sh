4eaf33c6e0303314aca281e8b3d0ac5550d48246
Adam Sawtell <110363956+AdamSawtell@users.noreply.github.com>
2023-02-12 16:42:30 +1030

    Create AbERP_Restart.sh

---
+++ AbERP_Restart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script Purpose: Restart AbilityERP (iDempiere) service - restart
+# Script Name: aberprestart.sh
+# Script Creater: Adam Sawtell
+# Created: 12/02/2023
+# Notes: 
+
+
+echo "flamingologic.com.au"
+
+echo "Restarting AblityERP (iDempiere)"
+
+echo "Stopping AbilityERP (iDempiere) Service."
+
+sudo service idempiere stop
+
+sleep 60
+
+echo "Starting AbilityERP (iDempiere) Service."
+
+sudo service idempiere start
+
+echo "flamingologic.com.au"
